a57670c2d08afaa1dbdf9fa314b1db24f751106f
Tianyu Wang <terrywang@tianyu.ninja>
2018-07-05 00:49:36 -0400

    Home finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,38 @@
+
 var $window = $(window);
 
 $(document).ready(function(){
-
-  $(".title-text").fitText(1, {minFontSize: '30px'});
-
-  if ($window.scrollTop() > 400){
+  if ($window.scrollTop() > 200){
     $("#header").addClass("bg-black");
   }
 
   $window.scroll(function(){
-    if ($(this).scrollTop() <= 400){
+    if ($(this).scrollTop() <= 200){
       $("#header").removeClass("bg-black");
     } else {
       $("#header").addClass("bg-black");
     }
   });
+
+  var title = new Typed('#title', {
+    strings: ['Hello World, I\'m Terry Wang'],
+    showCursor: false,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1500,
+    smartBackspace: false,
+    loop: false
+  })
+
+  var identity;
+  setTimeout(() => {
+    identity = new Typed('#identity', {
+      strings: ['Student @ UWaterloo.', 'Passionate Coder.', 'Full-Stack Developer.', 'Mobile Developer.' ],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1500,
+      smartBackspace: false,
+      loop: true
+    });
+  }, 3000);
 });
