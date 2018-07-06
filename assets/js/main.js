88a73d68eb2973f71ee73e23abb8c7cd584a4f7d
Tianyu Wang <terrywang@tianyu.ninja>
2018-07-05 22:22:28 -0400

    About me update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,46 @@ var $window = $(window);
 
 $(document).ready(function() {
   // navbar
+
   if ($window.scrollTop() > 200){
     $("#header").addClass("bg-black");
   }
 
-  $window.scroll(function(){
-    if ($(this).scrollTop() <= 200){
+  var toggle = false;
+  $window.scroll(function() {
+    if ($(this).scrollTop() <= 200 && !toggle){
       $("#header").removeClass("bg-black");
     } else {
       $("#header").addClass("bg-black");
     }
   });
+
+  $('.navbar-toggler').click(function() {
+    if ($window.scrollTop() <= 200 && toggle) {
+      $("#header").removeClass("bg-black");
+    } else {
+      $("#header").addClass("bg-black");
+    }
+    toggle = !toggle;
+  })
+
+  if ($window.width() < 768) {
+    $(".nav-item").removeClass("ml-2");
+    $(".nav-link").addClass("pl-3");
+  } else {
+    $(".nav-item").addClass("ml-2");
+    $(".nav-link").removeClass("pl-3");
+  }
+
+  $window.resize(function() {
+    if ($window.width() < 768) {
+      $(".nav-item").removeClass("ml-2");
+      $(".nav-link").addClass("pl-3");
+    } else {
+      $(".nav-item").addClass("ml-2");
+      $(".nav-link").removeClass("pl-3");
+    }
+  })
   //---
 
   $("a").on('click', function(event) {
@@ -32,7 +61,7 @@ $(document).ready(function() {
   // typed
 
   var identity = new Typed('#identity', {
-      strings: ['&nbsp', 'Student @ UWaterloo.', 'Passionate Coder.', 'Full-Stack Developer.', 'Mobile Developer.' ],
+      strings: ['&nbsp', 'Student @ UWaterloo.', 'Passionate Coder.', 'Full-Stack Developer.', 'Software Developer.', 'Mobile Developer.' ],
       typeSpeed: 100,
       backSpeed: 50,
       backDelay: 1500,
